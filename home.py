a30cd776a4350bbb92d22cf153beb97518449bb7
LucasGPestana <lucasgabrielpestana03@gmail.com>
2025-06-15 12:12:16 -0300

    Home v3

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pydeck as pdk
 import pandas as pd
 import geopandas as gpd
 import numpy as np
@@ -50,79 +51,104 @@ st.title('Previsão de preços de imóveis')
 
 counties = list(gdf_geo["name"].sort_values())
 
-selected_county = st.selectbox("Condado", counties)
+col1, col2 = st.columns(2)
 
-longitude = gdf_geo.query("name == @selected_county")["longitude"].values[0]
-latitude = gdf_geo.query("name == @selected_county")["latitude"].values[0]
+with col1:
 
-housing_median_age = st.number_input(
-    "Idade do imóvel", 
-    value=10, 
-    min_value=df["housing_median_age"].min(), 
-    max_value=df["housing_median_age"].max()
+    selected_county = st.selectbox("Condado", counties)
+
+    longitude = gdf_geo.query("name == @selected_county")["longitude"].values[0]
+    latitude = gdf_geo.query("name == @selected_county")["latitude"].values[0]
+
+    housing_median_age = st.number_input(
+        "Idade do imóvel", 
+        value=10, 
+        min_value=df["housing_median_age"].min(), 
+        max_value=df["housing_median_age"].max()
+        )
+
+    total_rooms = gdf_geo.query("name == @selected_county")["total_rooms"].values[0]
+    total_bedrooms = gdf_geo.query("name == @selected_county")["total_bedrooms"].values[0]
+    population = gdf_geo.query("name == @selected_county")["population"].values[0]
+    households = gdf_geo.query("name == @selected_county")["households"].values[0]
+
+    median_income = st.slider(
+        "Renda anual média, em US$", 
+        min_value=5_000.00, 
+        max_value=90_000.00,
+        value=45_000.00, 
+        step=0.01
+        ) / 10_000
+
+    ocean_proximity = gdf_geo.query("name == @selected_county")["ocean_proximity"].values[0]
+
+    rooms_per_households = gdf_geo.query("name == @selected_county")["rooms_per_households"].values[0]
+    population_per_households = gdf_geo.query("name == @selected_county")["population_per_households"].values[0]
+    bedrooms_per_rooms = gdf_geo.query("name == @selected_county")["bedrooms_per_rooms"].values[0]
+
+    median_income_cat_limits = np.append(np.arange(0, 7, 1.5), np.inf)
+    median_income_cat_labels = list(range(1, len(median_income_cat_limits)))
+
+    median_income_cat = getCategoryValue(
+        median_income,
+        limits=median_income_cat_limits,
+        labels=median_income_cat_labels
     )
 
-total_rooms = gdf_geo.query("name == @selected_county")["total_rooms"].values[0]
-total_bedrooms = gdf_geo.query("name == @selected_county")["total_bedrooms"].values[0]
-population = gdf_geo.query("name == @selected_county")["population"].values[0]
-households = gdf_geo.query("name == @selected_county")["households"].values[0]
 
-median_income = st.slider(
-    "Renda anual média, em US$", 
-    min_value=5_000.00, 
-    max_value=90_000.00,
-    value=45_000.00, 
-    step=0.01
-    ) / 10_000
+    housing_median_age_cat_limits = [0, 18, 30, 50, np.inf]
+    housing_median_age_cat_labels = list(range(1, len(housing_median_age_cat_limits)))
 
-ocean_proximity = gdf_geo.query("name == @selected_county")["ocean_proximity"].values[0]
+    housing_median_age_cat = getCategoryValue(
+        housing_median_age,
+        limits=housing_median_age_cat_limits,
+        labels=housing_median_age_cat_labels
+    )
 
-rooms_per_households = gdf_geo.query("name == @selected_county")["rooms_per_households"].values[0]
-population_per_households = gdf_geo.query("name == @selected_county")["population_per_households"].values[0]
-bedrooms_per_rooms = gdf_geo.query("name == @selected_county")["bedrooms_per_rooms"].values[0]
+    model_input = {
+        "longitude": longitude,
+        "latitude": latitude,
+        "housing_median_age": housing_median_age,
+        "total_rooms": total_rooms,
+        "total_bedrooms": total_bedrooms,
+        "population": population,
+        "households": households,
+        "median_income": median_income,
+        "ocean_proximity": ocean_proximity,
+        "rooms_per_households": rooms_per_households,
+        "population_per_households": population_per_households,
+        "bedrooms_per_rooms": bedrooms_per_rooms,
+        "median_income_cat": median_income_cat,
+        "housing_median_age_cat": housing_median_age_cat,
+    }
 
-median_income_cat_limits = np.append(np.arange(0, 7, 1.5), np.inf)
-median_income_cat_labels = list(range(1, len(median_income_cat_limits)))
+    df_model_input = pd.DataFrame(model_input, index=[0])
 
-median_income_cat = getCategoryValue(
-    median_income,
-    limits=median_income_cat_limits,
-    labels=median_income_cat_labels
-)
+    button_state = st.button("Prever preço")
 
+    if button_state:
 
-housing_median_age_cat_limits = [0, 18, 30, 50, np.inf]
-housing_median_age_cat_labels = list(range(1, len(housing_median_age_cat_limits)))
+        price = model.predict(df_model_input)
 
-housing_median_age_cat = getCategoryValue(
-    housing_median_age,
-    limits=housing_median_age_cat_limits,
-    labels=housing_median_age_cat_labels
-)
+        st.write(f"Preço: US$ {price[0]:.2f}")
 
-model_input = {
-    "longitude": longitude,
-    "latitude": latitude,
-    "housing_median_age": housing_median_age,
-    "total_rooms": total_rooms,
-    "total_bedrooms": total_bedrooms,
-    "population": population,
-    "households": households,
-    "median_income": median_income,
-    "ocean_proximity": ocean_proximity,
-    "rooms_per_households": rooms_per_households,
-    "population_per_households": population_per_households,
-    "bedrooms_per_rooms": bedrooms_per_rooms,
-    "median_income_cat": median_income_cat,
-    "housing_median_age_cat": housing_median_age_cat,
-}
+with col2:
 
-df_model_input = pd.DataFrame(model_input, index=[0])
+    view_state = pdk.ViewState(
+        latitude=latitude,
+        longitude=longitude,
+        zoom=5,
+        min_zoom=5,
+        max_zoom=15,
+    )
 
-button_state = st.button("Prever preço")
+    map_obj = pdk.Deck(
+        initial_view_state=view_state,
+        map_style="light"
+    )
 
-if button_state:
-
-    price = model.predict(df_model_input)
-
-    st.write(f"Preço: US$ {price[0]:.2f}")
+    st.pydeck_chart(
+        map_obj,
+        use_container_width=True,
+        height=500,
+        )
